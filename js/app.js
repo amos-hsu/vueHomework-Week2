45e72d0cd3742fbdd5353d2b0b6645b14e2edecc
amos-hsu <64068357+amos-hsu@users.noreply.github.com>
2022-01-18 17:08:15 +0800

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,37 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js';
+
+// 請加入站點
+const url = 'https://vue3-course-api.hexschool.io/v2';
+// 請加入個人 API Path
+const path = 'minhsin'; 
+
+const app = createApp({
+    data() {
+        return {
+            user: {
+                username: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        login() {
+            axios.post(`${url}/admin/signin`, this.user)
+                .then((res) => {
+                    console.log(res);
+                    const { token, expired } = res.data;
+                    console.log(token, expired);
+                    document.cookie = `hexToken=${token}; expires=${new Date(expired)};`;
+                    window.location = 'products.html';
+                })
+                .catch((err) => {
+                    alert(err.data.message);
+                });
+        }
+    },
+    mounted() {
+
+    }
+});
+
+app.mount('#app');
